c712c15285f1a4a2c188de1ad28f55e9939c5766
lounakamura <loukarinnakamura@gmail.com>
2023-01-03 21:37:32 +0100

    designing

--- index.php
+++ index.php
@@ -12,10 +12,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>The Roosevelt Hotel</title>
+    <link rel="icon" type="image/ico" href="images/ui/logo.svg">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="js/jquery-3.6.1.min.js"></script>
 </head>
 <body>
-    <h2>Elo test</h2>
+    <header>
+        <div class='header-bg'>
+            <div class='header-logo-container'>
+                <div class='header-logo-bg'>
+                    <h1>The Roosevelt Hotel</h1>
+                    <!-- ok so i want some parallax snapping -->
+                </div>
+            </div>
+        <div>
+    </header>
 </body>
 </html>
 
